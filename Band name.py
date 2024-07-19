c5b861ffc783e4d49788032a4d482b885b7a9b27
Ishwar Chaudhari <ishwarchaudharim@gmail.com>
2024-07-19 15:48:17 +0530

    Band name

---
+++ Band name.py
@@ -0,0 +1,14 @@
+#create a greeting for your program 
+print("Welcome ")
+#Ask for the first name
+name1=input("what is your first name\n")
+#Ask for last name 
+name2=input("what is your last name\n")
+#Ask name of ur state
+name3=input("Name of your state\n")
+#ASk where are u from
+name4=input("Name of your district\n")
+#Ask pin code of city
+name5=input("pincode of your city\n")
+#tell hello nice to meet you
+print("Hello Nice to meet u" " " + name1 + " " +  name2)
